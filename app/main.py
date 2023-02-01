b3c89075bbd7426db8b1bb29257a9e5cec6ce2f0
Naumova Sofya Alekseevna <isip_c.a.naumova@mpt.ru>
2023-02-01 10:43:02 +0300

    Added docker-compose.yml

---
+++ app/main.py
@@ -0,0 +1,26 @@
+# import calendar
+
+
+# print ('Добро пожаловать в календарь\n')
+
+
+# year = int (input('Введите год: '))
+
+# month = int (input('Введите номер месяца: '))
+
+
+# print(calendar.month(year, month))
+
+from pymongo import MongoClient
+
+from pprint import pprint
+
+MONGO_URL = "mongodb://mongo:27017"
+
+client = MongoClient(MONGO_URL)
+
+db = client.admin
+
+dbs_list = db.command("listDatabases")
+
+pprint(dbs_list)
